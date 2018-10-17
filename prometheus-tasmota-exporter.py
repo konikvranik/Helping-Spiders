b77627438e23637e632e8ecdb7a29d8e71e8eb65
vranikp <vranikp@lmc.eu>
2018-10-17 10:57:59 +0200

    simplify

--- prometheus-tasmota-exporter.py
+++ prometheus-tasmota-exporter.py
@@ -5,9 +5,19 @@ from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, StateSetMetricFamily, REGISTRY
 import pyjq
 import logging
+from systemd.journal import JournaldLogHandler
+
+logger = logging.getLogger(__name__)
+journald_handler = JournaldLogHandler()
+journald_handler.setFormatter(logging.Formatter(
+    '[%(levelname)s] %(message)s'
+))
+logger.addHandler(journald_handler)
+logger.setLevel(logging.DEBUG)
 
 statusQuery = '.StatusSTS | to_entries | map(select(.key | match("^POWER";"i"))) | map({key: .key, value: .value | test("ON") }) | from_entries'
 energyQuery = '.StatusSNS | select(.ENERGY) | .ENERGY | to_entries | map(select( .key == "Total" or .key == "Yesterday" or .key == "Today")) | from_entries'
+energyQuery = '.StatusSNS.ENERGY.Total'
 powerQuery = '.StatusSNS.ENERGY.Power'
 voltageQuery = '.StatusSNS.ENERGY.Voltage'
 currentQuery = '.StatusSNS.ENERGY.Current'
@@ -21,19 +31,20 @@ class TasmotaCollector(object):
     def collect(self):
         for u in self.urls:
             logging.info("Requesting JSON for %s" % u)
-            data = json.load(urllib2.urlopen("http://%s/cm?cmnd=status%%200" % u))
+            data = json.load(urllib2.urlopen(
+                "http://%s/cm?cmnd=status%%200" % u))
             tmp = self._collectStatus(u, data)
             if tmp:
-              yield tmp
+                yield tmp
             tmp = self._collectEnergy(u, data)
             if tmp:
-              yield tmp
+                yield tmp
             tmp = self._collectPower(u, data)
             if tmp:
-              yield tmp
+                yield tmp
             tmp = self._collectVoltage(u, data)
             if tmp:
-              yield tmp
+                yield tmp
 
     def _collectStatus(self, node, data):
         metric = StateSetMetricFamily(
@@ -49,26 +60,19 @@ class TasmotaCollector(object):
     def _collectEnergy(self, node, data):
         result = pyjq.first(energyQuery, data)
         logging.info("Energy: %s" % result)
-        if result:
-            metric = GaugeMetricFamily(
-                'energy',
-                'Energy reported by sensor',
-                labels=["node", "domain", "type", "unit", "context"])
-            for k,v in result.iteritems():
-                metric.add_metric([node, "sensor", "energy", "kWh", k], v)
+        if result != None:
+            metric = GaugeMetricFamily('energy', 'Energy reported by sensor',
+                                       labels=["node", "domain", "type", "unit"])
+            metric.add_metric([node, "sensor", "energy", "kWh"], result)
             return metric
 
     def _collectPower(self, node, data):
         result = pyjq.first(powerQuery, data)
         logging.info("Power: %s" % result)
         if result != None:
-            metric = GaugeMetricFamily(
-                'power',
-                'Power reported by sensor',
-                labels=["node", "domain", "type", "unit"])
-            print result
-            metric.add_metric(
-                [node, "sensor", "power", "W"], result)
+            metric = GaugeMetricFamily('power', 'Power reported by sensor',
+                                       labels=["node", "domain", "type", "unit"])
+            metric.add_metric([node, "sensor", "power", "W"], result)
             return metric
 
     def _collectVoltage(self, node, data):
@@ -79,7 +83,6 @@ class TasmotaCollector(object):
                 'voltage',
                 'Voltage reported by sensor',
                 labels=["node", "domain", "type", "unit"])
-            print result
             metric.add_metric(
                 [node, "sensor", "voltage", "V"], result)
             return metric
@@ -92,10 +95,10 @@ class TasmotaCollector(object):
                 'current',
                 'Current reported by sensor',
                 labels=["node", "domain", "type", "unit"])
-            print result
             metric.add_metric(
                 [node, "sensor", "current", "A"], result)
             return metric
+
 
 if __name__ == "__main__":
     REGISTRY.register(TasmotaCollector(

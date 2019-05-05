4f95e75048ef4523a7e87ecfa9f52562ac0ccd1f
Petr Vranik <petr@vranik.name>
2019-05-05 20:21:19 +0200

    name

--- esp06.h
+++ esp06.h
@@ -1,6 +1,6 @@
-#define SV "2.0"
-#define SN "ESP06"
-#define NODE_ID "ESP06"
+#define SV 2.0
+#define SN ESP06
+#define NODE_ID ESP06
 #define TEMP_CHILD_ID 2
 #define DS18_PIN 2
 #define HCSR04_TRIG_PIN 1
